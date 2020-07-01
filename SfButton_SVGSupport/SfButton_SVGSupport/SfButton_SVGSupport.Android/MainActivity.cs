c7774d7b1a5108123bf40b08019d3276c8e03fcf
AzureAD\HemalathaMarikumar <hemalatha.marikumar@syncfusion.com>
2020-07-01 21:47:59 +0530

    sample included

---
+++ SfButton_SVGSupport/SfButton_SVGSupport/SfButton_SVGSupport.Android/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace SfButton_SVGSupport.Droid
+{
+    [Activity(Label = "SfButton_SVGSupport", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Xamarin.Forms.Svg.Droid.SvgImage.Init(this);
+            LoadApplication(new App());
+        }
+    }
+}
