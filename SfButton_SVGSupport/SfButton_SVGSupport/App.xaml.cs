5f6b5449dd3ec53f58256542a99636d5f999b22d
Mohanlal Gopu <mohanlal.gopu@syncfusion.com>
2021-01-18 11:32:25 +0530

    Update new version.

---
+++ SfButton_SVGSupport/SfButton_SVGSupport/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Svg;
+using Xamarin.Forms.Xaml;
+
+namespace SfButton_SVGSupport
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            SvgImageSource.RegisterAssembly();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
